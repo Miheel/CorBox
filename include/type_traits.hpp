80e0b342715b873e8e6ddd0df3056008ed7b1763
Miheel <33738095+Miheel@users.noreply.github.com>
2023-11-16 02:30:43 +0100

    custom type trait urility remove extent and get pointer type

--- include/type_traits.hpp
+++ include/type_traits.hpp
@@ -24,6 +24,33 @@ namespace cor {
 	template<typename T>
 	using RemoveReference_T = typename RemoveReference<T>::Type;
 
+
+	template <typename T, typename D>
+	struct addPointer
+	{
+		typedef T* Type;
+	};
+	template<typename T, typename D>
+	using addPointer_T = typename addPointer<T, D>::Type;
+
+
+	template<class T>
+	struct RemoveExtent
+	{
+		typedef T Type;
+	};
+	template<class T>
+	struct RemoveExtent<T[]>
+	{
+		typedef T Type;
+	};
+	template<class T, size_t N>
+	struct RemoveExtent<T[N]>
+	{
+		typedef T Type;
+	};
+	template<typename T, typename D>
+	using RemoveExtent_T = typename RemoveExtent<T>::Type;
 } // !namespace cor
 
 #endif // !TYPE_TRAITS_HPP
