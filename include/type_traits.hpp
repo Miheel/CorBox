0094fcdf631616c89eef988f701832d7a83f36d3
Miheel <33738095+Miheel@users.noreply.github.com>
2023-11-23 01:57:39 +0100

    added metafunction enable_if and type trait is_array and integral_constant

--- include/type_traits.hpp
+++ include/type_traits.hpp
@@ -39,18 +39,74 @@ namespace cor {
 	{
 		typedef T Type;
 	};
+
 	template<class T>
 	struct RemoveExtent<T[]>
 	{
 		typedef T Type;
 	};
+
 	template<class T, size_t N>
 	struct RemoveExtent<T[N]>
 	{
 		typedef T Type;
 	};
-	template<typename T, typename D>
+
+	template<typename T>
 	using RemoveExtent_T = typename RemoveExtent<T>::Type;
+
+	template<bool B, class T = void>
+	struct EnableIf
+	{};
+
+	template<class T>
+	struct EnableIf<true, T>
+	{
+		typedef T Type;
+	};
+
+	template<bool B, class T = void>
+	using EnableIf_T = typename EnableIf<B, T>::Type;
+
+	template<class T, T val>
+	struct IntegralConstant
+	{
+		typedef T value_type;
+		typedef IntegralConstant type;
+
+		constexpr static T value = val;
+
+		constexpr operator value_type() const noexcept{
+			return value;
+		}
+
+		constexpr value_type operator()() const noexcept {
+			return value;
+		}
+	};
+
+	template<bool B>
+	using BoolConstant = IntegralConstant<bool, B>;
+
+	using TrueType = BoolConstant<true>;
+	using FalseType = BoolConstant<false>;
+
+	template<class T>
+	struct IsArray:FalseType
+	{};
+
+	template<class T>
+	struct IsArray<T[]> :TrueType
+	{};
+
+	template<class T, size_t N>
+	struct IsArray<T[N]> :TrueType
+	{};
+
+	template<class T>
+	inline constexpr bool IsArray_T = IsArray<T>::value;
+
+
 } // !namespace cor
 
 #endif // !TYPE_TRAITS_HPP
