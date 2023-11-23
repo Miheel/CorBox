ad8229d291f693782dfe6b5a9d3c7681d943d502
Miheel <33738095+Miheel@users.noreply.github.com>
2023-11-23 01:53:24 +0100

    more functions to allocate/dealocate memory. Allocate raw memory with operator new, alocate with placement new(place objects in alredy alocated storage. default Deleter class for arrays.

--- include/memory.hpp
+++ include/memory.hpp
@@ -2,6 +2,7 @@
 #define MEMORY_HPP
 
 #include "Util/utility.hpp"
+#include "type_traits.hpp"
 
 #ifdef _DEBUG
 #define _CRTDBG_MAP_ALLOC
@@ -10,21 +11,21 @@
 #include <iostream>
 #define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
 
+
 // Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
 // allocations to be of _CLIENT_BLOCK type
 #else
 #define DBG_NEW new
 #endif
 
-#undef new
-#include "MemoryLeakChecker.hpp"
-
+//#undef new
+//#include "MemoryLeakChecker.hpp"
 
 namespace cor::mem {
 
 	template <typename T>
 	T* allocateArr(size_t size) {
-		return new T[size];
+		return DBG_NEW T[size];
 	}
 
 	template <typename T>
@@ -33,8 +34,15 @@ namespace cor::mem {
 	}
 
 	template <typename T>
+	std::remove_extent_t<T>* allocate_r_extent(size_t size) {
+		typedef std::remove_extent_t<T> Elem;
+		Elem* alocatedMem = DBG_NEW Elem[size]();
+		return alocatedMem;
+	}
+	
+	template <typename T>
 	T* allocate() {
-		return new T;
+		return DBG_NEW T;
 	}
 
 	//template <typename T, typename U>
@@ -44,13 +52,42 @@ namespace cor::mem {
 
 	template <typename T, typename... U>
 	T* allocate(U&&... value) {
-		return new T(cor::forward<U>(value)...);
+		return DBG_NEW T(cor::forward<U>(value)...);
 	}
-
 
 	template <typename T>
 	void deallocate(T* ptr) {
 		delete ptr;
+	}
+
+	template <typename T, typename Place, typename... Args>
+	T* allocatePlace(Place* place, Args&&... args) {
+		return ::new (place) T(std::forward<Args>(args)...);
+	}
+
+	template<class T>
+	struct Deleter
+	{
+		constexpr Deleter() noexcept = default;
+
+		constexpr void operator()(T* ptr) const {
+			deallocate(ptr);
+		}
+	};
+
+	template<class T>
+	struct Deleter<T[]>
+	{
+		constexpr Deleter() noexcept = default;
+
+		template< class U >
+		constexpr void operator()(U* ptr) const {
+			deallocateArr(ptr);
+		}
+	};
+
+	void* allocateRaw(size_t count) {
+		return operator new(count);
 	}
 
 	template<typename InputIt, typename OutputIt>
@@ -60,16 +97,6 @@ namespace cor::mem {
 			*d_first = *first;
 		}
 	}
-
-	template<class T>
-	struct Deleter
-	{
-		constexpr Deleter() noexcept = default;
-
-		void operator()(T* ptr)const noexcept {
-			delete ptr;
-		}
-	};
 
 	template<typename InContainer, typename OutContainer>
 	void memCopy(InContainer& source, OutContainer& dest) {
