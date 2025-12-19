09243e47930fcaf34b1dcda347f49b5bb5c7b6ba
Miheel <33738095+Miheel@users.noreply.github.com>
2025-12-20 00:06:18 +0100

    moved using keyword for specializations of matrix. added vector norm for rowvec and vector. added cofactorexp for deternimant of matricies over size 4. equality op and vector div ops. style change and stricter row and col naming and ordering.

--- include/math/matrix.hpp
+++ include/math/matrix.hpp
@@ -1,174 +1,305 @@
 #ifndef MATRIX_HPP
 #define MATRIX_HPP
 
+#include <cmath>
 #include <iostream>
 #include <initializer_list>
+#include "math.hpp"
 #include "static_array.hpp"
 #include "type_traits.hpp"
 
 namespace cor
 {
+	template <typename T, cor::usize R, cor::usize C>
+	class MatX;
 
-	template <typename T, usize row, usize col>
+	using Mat2 = MatX<double, 2, 2>;
+	using Mat3 = MatX<double, 3, 3>;
+	using Mat4 = MatX<double, 4, 4>;
+	template <typename T, cor::usize C>
+	using RowVec = MatX<T, 1, C>;
+	template <typename T, cor::usize R>
+	using Vector = MatX<T, R, 1>;
+
+	template <typename T, cor::usize R, cor::usize C, cor::usize otherR, cor::usize otherC>
+	void CofactorExp(cor::usize colpos, MatX<T, R, C> &origmat, MatX<T, otherR, otherC> &newmat);
+
+	template <typename T, cor::usize R, cor::usize C>
+	T det(MatX<T, R, C> &mat);
+
+	template <typename T, cor::usize R, cor::usize C>
 	class MatX
 	{
 
 	public:
-		MatX() = default;
+		MatX()
+		{
+			data.fill(0);
+		}
 
 		MatX(std::initializer_list<T> list)
 		{
 			data = list;
 		}
 
-		T &operator()(usize rowpos, usize colpos)
+		MatX(const MatX &mat) : data(mat.data)
 		{
-			return data[rowpos * col + colpos];
-		}
-		const T &operator()(usize rowpos, usize colpos) const
-		{
-			return data[rowpos * col + colpos];
 		}
 
-		T &operator[](usize pos) { return data[pos]; }
-		const T &operator[](usize pos) const { return data[pos]; }
-
-		template <usize Lrow, usize Lcol>
-		MatX operator+(MatX<T, Lrow, Lcol> &lhs)
+		MatX(MatX &&mat) noexcept
 		{
-			assert((col == Lcol && row == Lrow) && "mat 1 and mat 2 must have equal # of col and row");
-			for (usize i = 0; i < row * col; i++)
-			{
-				data[i] += lhs[i];
-			}
+			this->data.swap(mat.data);
+		}
+
+		T &operator()(cor::usize rowpos, cor::usize colpos)
+		{
+			return data[rowpos * C + colpos];
+		}
+		const T &operator()(cor::usize rowpos, cor::usize colpos) const
+		{
+			return data[rowpos * C + colpos];
+		}
+
+		T &operator[](cor::usize pos) { return data[pos]; }
+		const T &operator[](cor::usize pos) const { return data[pos]; }
+
+		constexpr cor::usize rows() const { return R; }
+		constexpr cor::usize cols() const { return C; }
+
+		MatX &operator=(const MatX &rhs)
+		{
+			MatX temp(rhs);
+			this->data.swap(temp.data);
+			return *this;
+		}
+		MatX &operator=(MatX &&rhs) noexcept
+		{
+			this->data.swap(rhs.data);
 			return *this;
 		}
 
-		template <usize Lrow, usize Lcol>
-		MatX operator-(MatX<T, Lrow, Lcol> &lhs)
+		template <cor::usize otherR, cor::usize otherC>
+		MatX operator+(const MatX<T, otherR, otherC> &rhs) const
 		{
-			assert((col == Lcol && row == Lrow) && "mat 1 and mat 2 must have equal # of col and row");
-			for (usize i = 0; i < row * col; i++)
+			assert((C == otherC && R == otherR) && "mat 1 and mat 2 must have equal # of col and row");
+			MatX result;
+			for (cor::usize i = 0; i < R * C; i++)
 			{
-				data[i] -= lhs[i];
+				result[i] = data[i] + rhs[i];
 			}
-			return *this;
+			return result;
 		}
 
-		MatX operator*(usize scalar)
+		template <cor::usize otherR, cor::usize otherC>
+		MatX operator-(const MatX<T, otherR, otherC> &rhs) const
 		{
-			for (usize i = 0; i < row * col; i++)
+			assert((C == otherC && R == otherR) && "mat 1 and mat 2 must have equal # of col and row");
+
+			MatX result;
+			for (cor::usize i = 0; i < R * C; i++)
 			{
-				data[i] *= scalar;
+				result[i] = data[i] - rhs[i];
 			}
-			return *this;
+			return result;
 		}
 
-		template <usize Lrow, usize Lcol>
-		MatX<T, row, Lcol> operator*(MatX<T, Lrow, Lcol> &lhs)
+		MatX operator*(T scalar) const
 		{
-			assert((col == Lrow) && "mat 1 col must be the same a mat 2 row");
-			MatX<T, row, Lcol> newmat;
+			MatX result;
+			for (cor::usize i = 0; i < R * C; i++)
+			{
+				result[i] = data[i] * scalar;
+			}
+			return result;
+		}
+
+		template <typename U, cor::usize otherR, cor::usize otherC>
+		MatX<T, R, otherC> operator*(const MatX<U, otherR, otherC> &rhs) const
+		{
+			assert((C == otherR) && "mat1 col must be the same a mat2 row");
+			MatX<T, R, otherC> newmat;
 			T sum = 0;
-			for (usize _row = 0; _row < row; _row++)
+			for (cor::usize _row = 0; _row < R; _row++)
 			{
-				for (usize _col = 0; _col < Lcol; _col++)
+				for (cor::usize _col = 0; _col < otherC; _col++)
 				{
 					sum = 0;
-					for (usize i = 0; i < col; i++)
+					for (cor::usize i = 0; i < C; i++)
 					{
-						sum += data[_row * col + i] * lhs[i * Lcol + _col];
+						// sum += data[_row * C + i] * rhs[i * otherC + _col];
+						sum += this->operator()(_row, i) * rhs(i, _col);
 					}
-					newmat[_row * Lcol + _col] = sum;
+					// newmat[_row * otherC + _col] = sum;
+					newmat(_row, _col) = sum;
 				}
 			}
 			return newmat;
 		}
 
-		double det()
+		// determinant only works on square matricies where row == col
+		template <cor::usize origR = R, cor::usize origC = C, typename = cor::EnableIf_T<(origR == origC)>>
+		T det()
 		{
-			assert((col == row) && "determinant only works on quadratic matricies");
-			if constexpr (row == 2)
-			{
-				return data[0] * data[3] - data[1] * data[2];
-			}
-			if constexpr (row == 3)
-			{
-				return (*this)(0, 0) * (*this)(1, 1) * (*this)(2, 2) +
-					   (*this)(0, 1) * (*this)(1, 2) * (*this)(2, 0) +
-					   (*this)(0, 2) * (*this)(1, 0) * (*this)(2, 1) -
-					   (*this)(0, 2) * (*this)(1, 1) * (*this)(2, 0) -
-					   (*this)(0, 1) * (*this)(1, 0) * (*this)(2, 2) -
-					   (*this)(0, 0) * (*this)(1, 2) * (*this)(2, 1);
-			}
-			if constexpr (row > 3)
-			{
-				// do stuff
-			}
+			assert((C == R) && "determinant only works on square matricies");
+			return cor::det(*this);
 		}
 
-		MatX<T, col, row> Tanspose()
+		MatX<T, C, R> transpose() const
 		{
-			MatX<T, col, row> newmat;
+			MatX<T, C, R> newmat;
 
-			for (usize _row = 0; _row < row; _row++)
+			for (cor::usize _row = 0; _row < R; _row++)
 			{
-				for (usize _col = 0; _col < col; _col++)
+				for (cor::usize _col = 0; _col < C; _col++)
 				{
-					newmat[_col * row + _row] = (*this)(_row, _col);
+					newmat(_col, _row) = this->operator()(_row, _col);
 				}
 			}
 			return newmat;
+		}
+
+		void zero()
+		{
+			data.fill(0);
 		}
 
 		void print()
 		{
 
-			for (usize _row = 0; _row < row; _row++)
+			for (cor::usize _row = 0; _row < R; _row++)
 			{
-				for (usize _col = 0; _col < col; _col++)
+				for (cor::usize _col = 0; _col < C; _col++)
 				{
-					std::cout << data[_row * col + _col] << " ";
+					std::cout << this->operator()(_row, _col) << " ";
 				}
 				std::cout << "\n";
 			}
 		}
 
 	private:
-		cor::Static_Array<T, row * col> data;
+		cor::Static_Array<T, R * C> data;
 	};
 
-	using Mat2 = MatX<double, 2, 2>;
-	using Mat3 = MatX<double, 3, 3>;
-	template <typename T, usize size>
-	using RowVec = MatX<T, 1, size>;
-	template <typename T, usize size>
-	using Vector = MatX<T, size, 1>;
+	template <typename T, cor::usize R, cor::usize C, cor::usize otherR, cor::usize otherC>
+	void CofactorExp(cor::usize colpos, MatX<T, R, C> &origmat, MatX<T, otherR, otherC> &newmat)
+	{
+		int j = 0;
+		for (cor::usize row = 1; row < R; row++)
+		{
+			for (cor::usize col = 0; col < C; col++)
+			{
+				if (col != colpos)
+				{
+					newmat[j++] = origmat(row, col);
+				}
+			}
+		}
+	}
 
-	// template<class>
-	// class Array;
+	template <typename T, cor::usize R, cor::usize C>
+	T det(MatX<T, R, C> &mat)
+	{
+		assert((C == R) && "determinant only works on square matricies");
+		if constexpr (R == 2)
+		{
+			return mat[0] * mat[3] - mat[1] * mat[2];
+		}
+		if constexpr (R == 3)
+		{
+			return mat(0, 0) * mat(1, 1) * mat(2, 2) +
+				   mat(0, 1) * mat(1, 2) * mat(2, 0) +
+				   mat(0, 2) * mat(1, 0) * mat(2, 1) -
+				   mat(0, 2) * mat(1, 1) * mat(2, 0) -
+				   mat(0, 1) * mat(1, 0) * mat(2, 2) -
+				   mat(0, 0) * mat(1, 2) * mat(2, 1);
+		}
+		if constexpr (R > 3)
+		{
+			MatX<T, R - 1, C - 1> temp;
+			T determinant = 0;
+			int sign = 1;
+			for (cor::usize i = 0; i < R; i++)
+			{
+				if (R >= 4)
+				{
+					auto elem = mat(0, i);
+					CofactorExp(i, mat, temp);
+					determinant += sign * elem * det(temp);
+					sign = -sign;
+				}
+			}
+			return determinant;
+		}
+	}
 
-	// using Array2D = cor::Array<cor::Array<float>>;
+	template <typename T, cor::usize R, cor::usize C>
+	T EuclidVecNorm(const MatX<T, R, C> &vec)
+	{
+		T sum = 0;
+		for (cor::usize i = 0; i < R * C; i++)
+		{
+			sum += vec[i] * vec[i];
+		}
+		return std::sqrt(sum);
+	}
 
-	// Array2D identityMatrix(usize N);
+	template <typename T, cor::usize R, cor::usize C>
+	bool operator==(const MatX<T, R, C> &lhs, const MatX<T, R, C> &rhs)
+	{
+		for (cor::usize i = 0; i < R * C; i++)
+		{
+			if (lhs[i] != rhs[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 
-	// Array2D matrixAdd2D(Array2D& first, Array2D& second);
+	template <typename T, cor::usize C>
+	RowVec<T, C> operator/(const RowVec<T, C> &vec, double mag)
+	{
+		RowVec<T, C> normvec;
+		for (cor::usize i = 0; i < C; i++)
+		{
+			normvec[i] = vec[i] / mag;
+		}
+		return normvec;
+	}
 
-	// Array2D matrixSub2D(Array2D& first, Array2D& second);
+	template <typename T, cor::usize C>
+	RowVec<T, C> operator/(double mag, const RowVec<T, C> &vec)
+	{
+		return vec / mag;
+	}
 
-	// Array2D matrixScalar2D(Array2D& first, int scalar);
+	template <typename T, cor::usize R>
+	Vector<T, R> operator/(const Vector<T, R> &vec, double mag)
+	{
+		Vector<T, R> normvec;
+		for (cor::usize i = 0; i < R; i++)
+		{
+			normvec[i] = vec[i] / mag;
+		}
+		return normvec;
+	}
 
-	// Array2D matrixMultip2D(Array2D& first, Array2D& second);
+	template <typename T, cor::usize R>
+	Vector<T, R> operator/(double mag, const Vector<T, R> &vec)
+	{
+		return vec / mag;
+	}
 
-	// Array2D matrixTransp2D(Array2D& first);
+	// TOODO: implement eigenvalues and eigenvectors
+	//  hint: power iteration method
+	//  template <typename T, cor::usize R, cor::usize C>
+	//  MatX<double, R, C> eigen(MatX<double, R, C> &vec)
+	//  {
+	//  	MatX<double, R, C> eigenvec;
+	//  	eigenvec.zero();
 
-	// Array2D matrixinvers2D(Array2D& first);
-
-	// float Det2x2(Array2D& first);
-	// float Det3x3(Array2D& first);
-
-	// Array2D matrixDet2D(Array2D& first);
-
+	// 	eigenvec = vec * eigenvec;
+	// }
 }
 
 #endif // !MATRIX_HPP
